e4bf853809bb548696607b4999de7237663585cc
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-03 21:08:00 +0300

    'Solution'

---
+++ js_count-occurrences/src/countOccurrences.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Implement countOccurrences function:
+ *
+ * It takes a phrase and a letter and calculates the number of times
+ * the letter appears in the phrase. The function is case insensitive.
+ *
+ * countOccurrences('letter', 't') === 2
+ * countOccurrences('abc', 'a') === 1
+ * countOccurrences('abc', 'd') === 0
+ * countOccurrences('ABC', 'a') === 1
+ *
+ * @param {string} phrase
+ * @param {string} letter
+ *
+ * @return {number}
+ *
+ */
+function countOccurrences(phrase, letter) {
+  let timesOccur = 0;
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase.toLowerCase().charAt(i) === letter) {
+      phrase.charAt(i).slice(i);
+      timesOccur++;
+    }
+  }
+  return timesOccur;
+}
+
+module.exports = countOccurrences;
