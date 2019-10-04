3eb7b63f6f816986dc38bb5ec315fc6dfa047b92
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-04 13:56:23 +0300

    Solution

---
+++ js_invert-color/src/invertColor.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Write a function accepting 6 digit HEX color with a leading #
+ * and returning inverted color (color + inverted === white)
+ *
+ * invertColor('#000000') === '#FFFFFF' // 0x000000 + 0xFFFFFF === 0xFFFFFF
+ * invertColor('#DDEEAA') === '#221155' // 0x221155 + 0xDDEEAA === 0xFFFFFF
+ * invertColor('#012345') === '#FEDCBA' // 0x012345 + 0xFEDCBA === 0xFFFFFF
+ *
+ * @param {string} color
+ *
+ * @return {string}
+ */
+function invertColor(color) {
+  let hex = color;
+  if (hex.indexOf('#') === 0) {
+    hex = hex.slice(1);
+  }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+    throw new Error('Invalid HEX color.');
+  }
+  // invert color components
+  const r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16);
+  const g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16);
+  const b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+  // pad each with zeros and return
+  return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+  const length = len || 2;
+  const zeros = new Array(length).join('0');
+  return (zeros + str).slice(-length);
+}
+
+module.exports = invertColor;
