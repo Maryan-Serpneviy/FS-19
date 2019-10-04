099728c94e68d9c57a940dcd43bc72e0fd4e75c4
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-04 11:43:29 +0300

    Solution

---
+++ js_task-case-swapping/src/case-swapping.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Implement swapCase function:
+ *
+ * Given a string, swap the case for each of the letters.
+ *
+ * Examples:
+ * swapCase "aBc"      should return "AbC"
+ * swapCase "ABC"      should return "abc"
+ * swapCase "123235"   should return "123235"
+ *
+ * @param {string} phrase
+ *
+ * @return {string}
+ */
+function swapCase(phrase) {
+  let swappedPhrase = '';
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] === phrase[i].toLowerCase()) {
+      swappedPhrase += phrase[i].toUpperCase();
+    } else {
+      swappedPhrase += phrase[i].toLowerCase();
+    }
+  }
+  return swappedPhrase;
+}
+
+module.exports = swapCase;
