bd2c6611cea9ebcf9165d3db8e7d235bbb363a1f
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-07 16:59:49 +0300

    Solution

---
+++ js_deep-equal/readme.md
@@ -0,0 +1,5 @@
+# Deep Equal
+
+**Read the guideline before start**
+
+[Guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md)
---
+++ js_deep-equal/src/deepEqual.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * Implement deepEqual function:
+ *
+ * Non object types are compared with ===. Objects are equal if all the own
+ * enumerable properties are equal
+ *
+ * deepEqual(1, 2) === false
+ * deepEqual(10, 10) === true
+ * deepEqual('10', 10) === false
+ * deepEqual(0, false) === false
+ * deepEqual({test: 5}, {test: 5}) === true
+ * deepEqual({test: {abc: 5}}, {test: {abc: 5}}) === true
+ * deepEqual({test: {abc: 5}}, {test: {abc: 5, def: 4}}) === false
+ *
+ * @param {any} a
+ * @param {any} b
+ *
+ * @return {boolean}
+ */
+function deepEqual(a, b) {
+  if (a === b) {
+    return true;
+  } else if ((typeof a === 'object' && a !== null)
+          && (typeof b === 'object' && b !== null)) {
+    if (Object.keys(a).length !== Object.keys(b).length) { return false; }
+
+    for (const prop in a) {
+      if (b.hasOwnProperty(prop)) {
+        if (!deepEqual(a[prop], b[prop])) { return false; }
+      } else { return false; }
+    }
+    return true;
+  } else { return false; }
+}
+
+module.exports = deepEqual;
