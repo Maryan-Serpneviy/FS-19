4f87f534f4ff90ee957ffc5f769c8981ae79e413
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-07 20:05:19 +0300

    Solution

---
+++ js_check_existence/src/checkExistence.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Implement checkExistence function:
+ *
+ * Function takes an array and some element
+ * and should return `true` if element exist in the array, `false` if not
+ *
+ * `checkExistence([1, 5, 7], 5) === true`
+ * `checkExistence(['Bob', 'Tom'], 'John') === false`
+ *
+ * @param {[]} array
+ * @param {*} element
+ *
+ * @return {boolean}
+ */
+function checkExistence(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = checkExistence;
