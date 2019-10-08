138887ae84bd4b65e01f08c051fc53d1bd1e46ca
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-08 19:31:34 +0300

    Solution

---
+++ js_double-trouble/src/doubleTrouble.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Implement doubleTrouble function:
+ *
+ * Function takes an array of integers (array), and a target (target), you must
+ * find out if any two consecutive numbers in the array sum to target. If so,
+ * remove the second number. Work through the array from left to right.
+ *
+ * Example:
+ *
+ * const array = [1, 2, 3, 4, 5]
+ * const target = 3
+ *
+ * 1+2 = target, so remove 2. No other pairs = target, so rest of array remains:
+ *
+ * [1, 3, 4, 5]
+ *
+ * Return the resulting array.
+ *
+ * @param {number[]} numbers
+ * @param {number} target
+ *
+ * @return {number[]} - array of remained numbers
+ */
+function doubleTrouble(numbers, target) {
+  return numbers.reduce((prev, curr) => {
+    if (!prev.length || curr + prev[prev.length - 1] !== target) {
+      prev.push(curr);
+    }
+    return prev;
+  }, []);
+}
+
+module.exports = doubleTrouble;
