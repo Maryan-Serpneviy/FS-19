3409f84bc698f59927f0910361166c50564063c7
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-09 19:40:03 +0300

    Solution

--- js_array-method-map/src/arrayMethodMap.js
+++ js_array-method-map/src/arrayMethodMap.js
@@ -5,43 +5,11 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback, thisArg) {
-    if (this == null) {
-      throw new TypeError(' this is null or not defined');
+    const m = [];
+    for (let i = 0; i < this.length; i++) {
+      m[i] = callback.call(thisArg, this[i], i, this);
     }
-    if (typeof callback !== 'function') {
-      throw new TypeError(callback + ' is not a function');
-    }
-    let T, k;
-
-    const O = Object(this);
-
-    // Нехай len дорівнює ToUint32(lenValue).
-    const len = O.length >>> 0;
-
-    if (arguments.length > 1) {
-      T = thisArg;
-    }
-
-    const A = new Array(len);
-    k = 0;
-
-    while (k < len) {
-      let kValue, mappedValue;
-      if (k in O) {
-        // і. Нехай kValue дорівнює результату виклику внутрішнього методу O
-        //    Get з аргументом k.
-        kValue = O[k];
-
-        // ii. Нехай mappedValue дорівнює результату виклику внутрішнього
-        //     методу callback Call з T у якості значення this та списком
-        //     аргументів, що містить kValue, k та O.
-        mappedValue = callback.call(T, kValue, k, O);
-
-        A[k] = mappedValue;
-      }
-      k++;
-    }
-    return A;
+    return m;
   };
 }
 
