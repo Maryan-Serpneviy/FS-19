91eae3dccb2cf53515a86ba866ff34d3ddeef531
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-10 08:10:19 +0300

    Solution

--- js_array-method-filter/src/arrayMethodFilter.js
+++ js_array-method-filter/src/arrayMethodFilter.js
@@ -4,41 +4,14 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
-  [].__proto__.filter2 = function(func, thisArg) {
-    if (!((typeof func === 'function') && this) ) {
-      throw new TypeError();
-    }
-
-    const len = this.length >>> 0;
-    const res = new Array(len); // попередньо створений масив
-    const t = this;
-    let c = 0;
-    let i = -1;
-
-    let kValue;
-    if (thisArg === undefined) {
-      while (++i !== len) {
-        // перевіряє, чи заданий ключ
-        if (i in this) {
-          kValue = t[i]; // у цьому випадку t змінюється у функції callback
-          if (func(t[i], i, t)) {
-            res[c++] = kValue;
-          }
-        }
-      }
-    } else {
-      while (++i !== len) {
-        // перевіряє, чи заданий ключ
-        if (i in this) {
-          kValue = t[i];
-          if (func.call(thisArg, t[i], i, t)) {
-            res[c++] = kValue;
-          }
-        }
+  [].__proto__.filter2 = function(callback, thisArg) {
+    const f = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback.call(thisArg, this[i], i, this)) {
+        f.push(this[i]);
       }
     }
-    res.length = c; // зменшити масив до правильного розміру
-    return res;
+    return f;
   };
 }
 
