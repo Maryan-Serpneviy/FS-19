250e36e593df663c1abab2f565bfb672292f8fa7
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-12 17:18:46 +0300

    Solution

---
+++ js_is-palindrome/src/isPalindrome.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Implement isPalindrome function:
+ *
+ * A palindrome is a word, phrase, or other sequence of characters which reads
+ * the same backward as forward, such as 'madam' or 'racecar'
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases. Empty string is a valid
+ * palindrome.
+ *
+ *
+ * isPalindrome('A man, a plan, a canal: Panama') === true
+ * isPalindrome('race a car') === false
+ *
+ * @param {string} str
+ *
+ * @return {boolean}
+ */
+function isPalindrome(str) {
+  const re = /[\W_]/g;
+  const lowCaseStr = str.toLowerCase().replace(re, '');
+  const reversedStr = lowCaseStr.split('').reverse().join('');
+  return lowCaseStr === reversedStr;
+}
+
+module.exports = isPalindrome;
