b18427eaacfa11fd9e5c2d7571e94ecda1f73523
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-14 20:17:45 +0300

    Solution

---
+++ js_closures-sum-with-closure/readme.md
@@ -0,0 +1,5 @@
+# Sum with a closure
+
+**Read the guideline before start**
+
+[Guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md)
---
+++ js_closures-sum-with-closure/src/sum.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Implement sum function:
+ *
+ * Function works with next pattern: sum(a)(b) === a + b. Double braces is not a
+ * mistake. The implementation requires solution with closures.
+ *
+ * sum(1)(2) === 3
+ * sum(2)(11) === 13
+ * sum(5)(-5) === 0
+ * sum(-10)(-1) === -11
+ *
+ * @param {number} a
+ *
+ * @return {function}
+ */
+function sum(a) {
+  return function add(b) {
+    return a + b;
+  };
+}
+
+module.exports = sum;
