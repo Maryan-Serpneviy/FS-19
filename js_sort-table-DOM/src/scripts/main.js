2ce5d695d9f4d44b507ffbf6b254e5bb3396ca45
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-19 11:20:55 +0300

    Advanced

--- js_sort-table-DOM/src/scripts/main.js
+++ js_sort-table-DOM/src/scripts/main.js
@@ -2,6 +2,86 @@
 
 const table = document.querySelector('[data-sortable]');
 
+const data = [
+    { brand: 'BMW', model: 'M3', year: 2017 },
+    { brand: 'Audi', model: 'Q5', year: 2018 },
+    { brand: 'Mercedes', model: 'S63', year: 2019 },
+    { brand: 'Volkswagen', model: 'Phaeton', year: 2012 },
+    { brand : 'Porsche', model: 'Panamera', year: 2016 }
+];
+
+const dataContainer = document.querySelector('#table');
+render(dataContainer, data);
+
+function createTable(el, data) {
+    let currField = null;
+    let currSign = 1;
+
+    render(el, data);
+
+    el.addEventListener('click', e => {
+        const headerCell = e.target.closest('[data-sort-filed]');
+
+        if (!headerCell) { return; }
+
+        const { sortField } = head.dataset;
+
+        if (sortField === currField) { currSign = -currSign; } else {
+            currSign = 1;
+        }
+
+        currField = sortField;
+
+        const sortedData = sortDataBy(data, sortField, currSign);
+
+        render(el, sortedData);
+    });
+}
+
+function sortDataBy(data, field, sign = 1) {
+    if (data.length === 0) { return []; }
+    
+    const sortType = typeof data[0][field];
+
+    return [...data].sort((a, b) => {
+        switch (sortType) {
+            case 'number':
+                return sign * (a[field] - b[field]);
+            case 'string':
+                return sign * (a[field].localeCompare(b[field]));
+            default:
+                return 0;
+        }
+    });
+}
+
+function renderRows(data) {
+    return data.map(el => `
+        <tr>
+            <td>${data.brand}</td>
+            <td>${data.model}</td>
+            <td>${dta.year}</td>
+        </tr>
+    `)
+    .join('');
+}
+
+function render(el, data) {
+    el.innerHTML = `
+    <table id="cars">
+      <thead>
+        <tr>
+            <th data-sort-field="brand">Brand</th>
+            <th data-sort-field="model">Model</th>
+            <th data-sort-field="year">Year</th>
+        </tr>
+      </thead>
+      <tbody data-rows-container>
+        ${renderRows(data)}
+      </tbody>
+    </table>`;
+}
+
 function sortTable(table) {
     const head = table.querySelector('[data-header]');
     const rowsContainer = table.querySelector('[data-rows-container]');
