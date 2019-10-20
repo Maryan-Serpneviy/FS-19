899f7bd95123f14cdeb9239edf0ed00d11b3b0c0
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-20 10:56:10 +0300

    Advanced

--- js_DOM-gallery/src/scripts/main.js
+++ js_DOM-gallery/src/scripts/main.js
@@ -4,14 +4,55 @@ const gallery = document.querySelector('.gallery');
 const imgContainer = gallery.querySelector('.gallery__list')
 const largeImg = gallery.querySelector('#largeImg');
 
-imgContainer.addEventListener('click', function(e) {
-    if (!e.target.classList.contains('gallery__img')) {
-        return;
-    }
-    e.preventDefault();
-    Array.prototype.forEach.call(this.children, li => {
+let currIndex = 0;
+
+const prevInactive = () => {
+    Array.prototype.forEach.call(imgContainer.children, li => {
         li.classList.remove('active');
     });
+};
+
+const toggleNextPic = () => {
+    if (currIndex >= imgContainer.children.length - 1) { return; }
+    prevInactive();
+    currIndex++;
+    const next = imgContainer.children[currIndex].querySelector('.gallery__img').src;
+    imgContainer.children[currIndex].classList.add('active');
+    largeImg.src = next.replace('jpeg', 'png').replace('-thumb', '');
+};
+
+const togglePrevPic = () => {
+    if (currIndex <= 0) { return; }
+    prevInactive();
+    const prev = imgContainer.children[currIndex - 1].querySelector('.gallery__img').src;
+    imgContainer.children[currIndex - 1].classList.add('active');
+    largeImg.src = prev.replace('jpeg', 'png').replace('-thumb', '');
+    currIndex--;
+};
+
+const defineKey = e => {
+    if (e.defaultPrevented) { return; }
+    
+    const PrevKey = new Set(['ArrowLeft', 'ArrowDown', 'a', 'A', 's', 'S', 'ф', 'Ф', 'ы', 'Ы', 'і', 'І']);
+    const NextKey = new Set(['ArrowRight', 'ArrowUp', 'd', 'D', 'w', 'W', 'в', 'В', 'ц', 'Ц']);
+
+    if (PrevKey.has(e.key)) { togglePrevPic(); }
+    if (NextKey.has(e.key)) { toggleNextPic(); }
+};
+
+imgContainer.addEventListener('click', e => {
+    if (!e.target.classList.contains('gallery__img')) { return; }
+    e.preventDefault();
+
+    currIndex =  e.target.parentNode.parentNode.id.slice(length - 1);
+
+    prevInactive();
     e.target.parentNode.parentNode.classList.add('active');
     largeImg.src = e.target.src.replace('jpeg', 'png').replace('-thumb', '');
+});
+
+document.addEventListener('keydown', defineKey);
+
+document.addEventListener('wheel', function(e) {
+    e.deltaY > 0 ? toggleNextPic() : togglePrevPic();
 });
