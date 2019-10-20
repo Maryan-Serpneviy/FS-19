7c8840d524f3b1f19d2c29f9d410d034cca29633
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-10-20 17:47:56 +0300

    Prototype

--- js_DOM-carousel/src/scripts/main.js
+++ js_DOM-carousel/src/scripts/main.js
@@ -5,67 +5,76 @@ const carouselPrev = carouselNode.querySelector('.carousel__btn_prev');
 const carouselNext = carouselNode.querySelector('.carousel__btn_next');
 const carouselDots = carouselNode.querySelectorAll('.carousel__dot');
 
-let currIndex = 0;
+function Carousel(prev, next, dots) {
+    this.prev = prev;
+    this.next = next;
+    this.dots = dots;
+    this.currIndex = 0;
+}
 
-const prevInactive = () => {
-    Array.prototype.forEach.call(carouselDots, dot => {
-        dot.classList.remove('carousel__dot_active');
-    });
-};
+Carousel.prototype = {
+    prevInactive() {
+        Array.prototype.forEach.call(carouselDots, dot => {
+            dot.classList.remove('carousel__dot_active');
+        });
+    },
+    
+    hideCurrent() {
+        this.prevInactive();
+        const curr = document.getElementById(`carousel-item-${this.currIndex}`);
+        curr.classList.remove('visible');
+    },
+    
+    showNext() {
+        const next = document.getElementById(`carousel-item-${this.currIndex}`);
+        next.classList.add('visible');
+        carouselDots[this.currIndex].classList.add('carousel__dot_active');
+    },
+    
+    onDotClick(target) {
+        if (target.classList.contains('carousel__dot_active')) { return; }
+        this.hideCurrent();
+        this.currIndex = target.id.slice(length - 1);
+        this.showNext();
+    },
+    
+    toggleNext() {
+        if (this.currIndex >= carouselDots.length - 1) { return; }
+        this.hideCurrent();
+        this.currIndex++;
+        this.showNext();
+    },
+    
+    togglePrev() {
+        if (this.currIndex <= 0) { return; }
+        this.hideCurrent();
+        this.currIndex--;
+        this.showNext();
+    },
 
-const hideCurrent = () => {
-    prevInactive();
-    const curr = document.getElementById(`carousel-item-${currIndex}`);
-    curr.classList.remove('visible');
-};
+    defineKey(e) {
+        e.preventDefault();
+        
+        const PrevKey = new Set(['ArrowLeft', 'ArrowDown', 'a', 'A', 's', 'S', 'ф', 'Ф', 'ы', 'Ы', 'і', 'І']);
+        const NextKey = new Set(['ArrowRight', 'ArrowUp', 'd', 'D', 'w', 'W', 'в', 'В', 'ц', 'Ц']);
+    
+        if (PrevKey.has(e.key)) { carousel.togglePrev(); }
+        if (NextKey.has(e.key)) { carousel.toggleNext(); }
+    }
+}
 
-const showNext = () => {
-    const next = document.getElementById(`carousel-item-${currIndex}`);
-    next.classList.add('visible');
-    carouselDots[currIndex].classList.add('carousel__dot_active');
-};
-
-const onDotClick = target => {
-    if (target.classList.contains('carousel__dot_active')) { return; }
-    hideCurrent();
-    currIndex = target.id.slice(length - 1);
-    showNext();
-};
-
-const toggleNext = () => {
-    if (currIndex >= carouselDots.length - 1) { return; }
-    hideCurrent();
-    currIndex++;
-    showNext();
-};
-
-const togglePrev = () => {
-    if (currIndex <= 0) { return; }
-    hideCurrent();
-    currIndex--;
-    showNext();
-};
+const carousel = new Carousel(carouselPrev, carouselNext, carouselDots);
 
 carouselNode.addEventListener('click', e => {
-    if (e.target === carouselPrev) { togglePrev(); }
-    if (e.target === carouselNext) { toggleNext() }
+    if (e.target === carouselPrev) { carousel.togglePrev(); }
+    if (e.target === carouselNext) { carousel.toggleNext() }
     if (e.target.classList.contains('carousel__dot')) {
-        onDotClick(e.target);
+        carousel.onDotClick(e.target);
     }
 });
 
-const defineKey = e => {
-    if (e.defaultPrevented) { return; }
-    
-    const PrevKey = new Set(['ArrowLeft', 'ArrowDown', 'a', 'A', 's', 'S', 'ф', 'Ф', 'ы', 'Ы', 'і', 'І']);
-    const NextKey = new Set(['ArrowRight', 'ArrowUp', 'd', 'D', 'w', 'W', 'в', 'В', 'ц', 'Ц']);
-
-    if (PrevKey.has(e.key)) { togglePrev(); }
-    if (NextKey.has(e.key)) { toggleNext(); }
-};
-
-document.addEventListener('keydown', defineKey);
+document.addEventListener('keydown', carousel.defineKey);
 
 document.addEventListener('wheel', e => {
-    e.deltaY > 0 ? toggleNext() : togglePrev();
+    e.deltaY > 0 ? carousel.toggleNext() : carousel.togglePrev();
 });
