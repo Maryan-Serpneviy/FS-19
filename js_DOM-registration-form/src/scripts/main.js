bcc458beeb24a4c089a2d3d2c26eee0ed43dae46
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-11-12 10:43:23 +0200

    Solution

--- js_DOM-registration-form/src/scripts/main.js
+++ js_DOM-registration-form/src/scripts/main.js
@@ -1,44 +1,114 @@
-'use strict';
+import { Region } from './region.js';
 
 const form = document.querySelector('#registration-form');
 const name = form.querySelector('#name');
 const phone = form.querySelector('#phone');
+const region = form.querySelector('#region');
+const city = form.querySelector('#city');
+const anonymous = form.querySelector('#private');
+const submit = form.querySelector('#submit');
 
-let isValidName = false;
+let validName = false;
+let validPhone = false;
+let regionSelected = false;
+let citySelected = false;
+let isAnonymous = false;
 
-const Registration = {
-    name: {
-        isValidName: false
-    }
+if (window.innerWidth / window.innerHeight < 1) {
+    form.style.width = `${window.innerWidth}px`;
+}
 
-};
 // name
 name.addEventListener('input', function() {
+    // (\w{4,}) (\w{4,}) ?(\w{4,})?
     const nameParts = this.value.trim().split(' ')
         .filter(part => part !== '')
         .map(part => part.trim());
-    isValidName = nameParts.every(el => el.length >= 4);
-    if (isValidName && (nameParts.length === 2 || nameParts.length === 3)) {
+    validName = nameParts.every(el => el.length >= 4);
+    if (validName && (nameParts.length === 2 || nameParts.length === 3)) {
+        validName = true;
         this.className = 'valid';
     } else {
+        validName = false;
         this.className = 'invalid';
-        isValidName = false;
     } 
 });
 
 name.addEventListener('focus', function() {
-    if (isValidName) {
-        this.className = 'valid';
-    } else this.className = 'invalid';
+    validName ? this.className = 'valid' : this.className = 'invalid';
 });
-
-// name.addEventListener('blur', function() {
-//     if (isValidName) {
-//         this.className = 'nostate';
-//     }
-// });
 
 // phone
 phone.addEventListener('input', function() {
-    console.log(this.value)
+    const mobile = /\+?(\d{2}) ?(\(|-| )?[0-9](\-| )?(\d{2})(\)|\-| )?(\-| )?(\d{3})(\-| )?(\d{2})(\-| )?(\d{2})/;
+    if (this.value.match(mobile)) {
+        validPhone = true;
+        this.className = 'valid';
+    } else {
+        validPhone = false;
+        this.className = 'invalid';
+    }
 });
+
+phone.addEventListener('focus', function() {
+    this.value = '+380 ';
+    validPhone ? this.className = 'valid' : this.className = 'invalid';
+});
+
+// region
+region.addEventListener('change', function() {
+    if (this.value !== this.children[0].textContent &&
+        this.value !== this.children[1].textContent) {
+
+        regionSelected = true;
+        this.className = 'valid';
+        city.classList.remove('hidden');
+        city.innerHTML = Region[this.value];
+    } else if (this.value === this.children[1].textContent) {
+        regionSelected = true;
+        this.className = 'valid';
+        city.classList.add('hidden');
+    } else {
+        regionSelected = false;
+        this.className = 'invalid';
+        city.classList.add('hidden');
+    }
+});
+
+// city
+city.addEventListener('change', function() {
+    if (this.value !== this.children[0].textContent) {
+        citySelected = true;
+        this.className = 'valid'
+    } else {
+        citySelected = false;
+        this.className = 'invalid';
+    }
+});
+
+// private
+anonymous.addEventListener('change', function() {
+    if (!phone.disabled) {
+        validPhone = true;
+        phone.disabled = true;
+        phone.className = 'disabled';
+        phone.value = 'Not specified';
+    } else {
+        validPhone = false;
+        phone.disabled = false;
+        phone.className = 'nostate';
+        phone.value = '';
+    }
+});
+
+// submit
+submit.disabled = true;
+submit.style.background = 'lightgray';
+
+form.addEventListener('change', function() {
+    if (validName && validPhone && regionSelected && citySelected) {
+        submit.disabled = false;
+        submit.style = 'border: 5px solid #28a745; background: #fff';
+    }    
+});
+
---
+++ js_DOM-registration-form/src/scripts/region.js
@@ -0,0 +1,40 @@
+export const Region = {
+    West: `
+        <option>Choose city</option>
+        <option>Lviv</option>
+        <option>Lutsk</option>
+        <option>Ivano-Frankivsk</option>
+        <option>Ternopil</option>
+        <option>Rivne</option>
+        <option>Chernivtsi</option>
+        <option>Khmelnytskyi</option>
+        <option>Uzhhorod</option>
+        <option><i>Not in the list</i></option>`,
+    Center: `
+        <option>Choose city</option>
+        <option>Cherkasy</option>
+        <option>Dnipro</option>
+        <option>Kropyvnytskyi</option>
+        <option>Poltava</option>
+        <option>Vinnytsia</option>
+        <option>Zhytomyr</option>
+        <option>Not in the list</option>`,
+    North: `
+        <option>Choose city</option>
+        <option>Chernihiv</option>
+        <option>Sumy</option>
+        <option>Not in the list</option>`,
+    South: `
+        <option>Choose city</option>
+        <option>Kherson</option>
+        <option>Mykolaiv</option>
+        <option>Zaporizhzhia</option>
+        <option>Odesa</option>
+        <option>Not in the list</option>`,
+    East: `
+        <option>Choose city</option>
+        <option>Kharkiv</option>
+        <option>Donetsk</option>
+        <option>Lugansk</option>
+        <option>Not in the list</option>`
+};
