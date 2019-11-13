35d2e8a0f56cecf00b7673ba5a965f015c87c066
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-11-13 13:05:12 +0200

    Solution

---
+++ js_days-in-year/index.js
@@ -0,0 +1,19 @@
+function daysInYear(year) {
+    try {
+        Number.isInteger(year);
+    } catch (e) {
+        throw new Error('Not an integer');
+    } 
+    return isLeapYear(year) ? 366 : 365;
+}
+
+function isLeapYear(year) {
+    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+daysInYear(2019); // 365
+daysInYear('2019'); // exception
+daysInYear(2020); // 366
+daysInYear(2020.2); // exception
+daysInYear(2100); // 365
+daysInYear({year: 2100}); // exception
