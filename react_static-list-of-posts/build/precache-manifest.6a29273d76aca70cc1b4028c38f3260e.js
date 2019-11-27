9705cbfeddb0b8267704a06c09fc96315c969227
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-11-27 13:50:32 +0200

    Build

---
+++ react_static-list-of-posts/build/precache-manifest.6a29273d76aca70cc1b4028c38f3260e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db8877f72a34aba908a20c5e7a3af2c",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "ff37b67ee509140714c2",
+    "url": "/react_static-list-of-posts/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/react_static-list-of-posts/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "ff37b67ee509140714c2",
+    "url": "/react_static-list-of-posts/static/js/main.8e1821db.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
