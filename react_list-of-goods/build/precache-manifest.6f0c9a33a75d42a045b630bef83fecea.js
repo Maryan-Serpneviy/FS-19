21ccc5a3bd606cf87ace7a30e1de2ad466ffc9c2
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-11-27 13:43:15 +0200

    Build

---
+++ react_list-of-goods/build/precache-manifest.6f0c9a33a75d42a045b630bef83fecea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56eecfe8952b233de40a9a61b040378d",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "d96721cf09499d1506cb",
+    "url": "/react_list-of-goods/static/css/main.9e97198e.chunk.css"
+  },
+  {
+    "revision": "2a032cb8a3c2e81f734b",
+    "url": "/react_list-of-goods/static/js/2.dbdfff11.chunk.js"
+  },
+  {
+    "revision": "d96721cf09499d1506cb",
+    "url": "/react_list-of-goods/static/js/main.8a075792.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
