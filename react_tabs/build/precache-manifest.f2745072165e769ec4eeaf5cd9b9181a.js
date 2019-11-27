9a535588c1033d2bb7caf15fd1df4b7d01275468
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-11-27 14:16:48 +0200

    Build

---
+++ react_tabs/build/precache-manifest.f2745072165e769ec4eeaf5cd9b9181a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7d79202b1d53525564a5131ec23b07",
+    "url": "/FS-19/index.html"
+  },
+  {
+    "revision": "59da4a99a2cd8cc8b805",
+    "url": "/FS-19/static/css/main.b39af6e2.chunk.css"
+  },
+  {
+    "revision": "688f50d758f0748bf2a8",
+    "url": "/FS-19/static/js/2.6ad9597c.chunk.js"
+  },
+  {
+    "revision": "59da4a99a2cd8cc8b805",
+    "url": "/FS-19/static/js/main.00fa74fd.chunk.js"
+  },
+  {
+    "revision": "38763567cc364fc6e5a5",
+    "url": "/FS-19/static/js/runtime~main.5ca397da.js"
+  }
+]);
