835544f92d2ba2b945d2066c1fbe22ca0d088ea0
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-11-30 19:20:36 +0200

    Init

---
+++ react_todo-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './assets/scss/App.scss'
+import TodoList from './components/TodoList/TodoList'
+
+export default function App() {
+  return (
+    <TodoList />
+  )
+}
