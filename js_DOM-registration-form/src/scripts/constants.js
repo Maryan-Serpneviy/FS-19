5770430b6f0bbf0fbb009fa2459c880cfb8d56f5
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-12-02 17:21:30 +0200

    Regex fix

--- js_DOM-registration-form/src/scripts/constants.js
+++ js_DOM-registration-form/src/scripts/constants.js
@@ -1,7 +1,7 @@
 const Const = {
     MAX_DIGITS: 14,
     RegExp: {
-        name: /^(\w{4,}) (\w{4,}) ?(\w{4,})?$/,
+        name: /^([A-zА-я]{4,}) ([A-zА-я]{4,}) ?([A-zА-я]{4,})?$/,
         phone: /\+?(\d{2}) ?(\(|-| )?[0-9](-| )?(\d{2})(\)|-| )?(-| )?(\d{3})(-| )?(\d{2})(-| )?(\d{2})$/
     },
     valid: 'border: 5px solid #28a745; background: #fff'
