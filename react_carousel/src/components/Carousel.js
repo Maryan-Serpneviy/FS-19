496dbd573f4689bdfddaa73f95e9e6944de7cfbd
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-12-02 19:22:42 +0200

    Dried

--- react_carousel/src/components/Carousel.js
+++ react_carousel/src/components/Carousel.js
@@ -5,20 +5,18 @@ import images from './images'
 const Option = {
     itemsCount: images.length,
     itemWidth: 100,
-    interval: 2500
+    interval: 2500,
+    maxSize: 5,
+    maxStep: 5
 }
 
 export default class Carousel extends React.Component {
     state = {
-        options: false,
         frameSize: 3,
         step: 3,
         loopInterVal: Option.interval,
-        infinite: false
+        infinite: true
     }
-
-    framesCounter = Option.itemsCount - this.state.frameSize
-    scrollCounter = 0
     // refs
     carousel = React.createRef()
     itemsContainer = React.createRef()
@@ -28,6 +26,9 @@ export default class Carousel extends React.Component {
     size = React.createRef()
     step = React.createRef()
     interval = React.createRef()
+
+    framesCounter = Option.itemsCount - this.state.frameSize
+    scrollCounter = 0
 
     scrollBackwards = () => {
         this.enableBtn(this.next)
@@ -43,7 +44,6 @@ export default class Carousel extends React.Component {
         if (this.framesCounter >= Option.itemsCount - this.state.frameSize) {
             this.framesCounter = Option.itemsCount - this.state.frameSize
             this.scrollCounter = 0
-
             this.disableBtn(this.prev)
         }
     }
@@ -62,7 +62,6 @@ export default class Carousel extends React.Component {
         if (this.framesCounter <= 0) {
             this.framesCounter = 0
             this.scrollCounter = Option.itemsCount * Option.itemWidth - this.state.frameSize * Option.itemWidth // also subtract last frames group
-
             this.disableBtn(this.next)
         }
     }
@@ -72,44 +71,29 @@ export default class Carousel extends React.Component {
         if (this.framesCounter === 0) {
             this.scrollCounter = -(Option.itemWidth * this.state.frameSize)
             this.framesCounter = Option.itemsCount
-
             this.enableBtn(this.next)
         }
     }
 
     toggleLoop = () => {
-        if (!this.state.infinite) {
-            this.state.infinite = true
-            this.loop.current.style.boxShadow = 'inset 0 0 5px 2px rgb(134, 134, 11)'
-            this.loopInterval = setInterval(this.loopCallback, this.state.loopInterVal)
-        } else {
-            this.state.infinite = false
-            this.loop.current.style.boxShadow = ''
-            clearInterval(this.loopInterval)
-        }
-        this.setState({
-            options: !this.state.options
-        })
+        this.setState({ infinite: !this.state.infinite })
+        this.state.infinite
+            ? this.loopInterval = setInterval(this.loopCallback, this.state.loopInterVal)
+            : clearInterval(this.loopInterval)
     }
 
-    handleFramesChange = e => {
-        this.setState({
-            frameSize: e.target.value
-        })
-    }
-
-    handleStepChange = e => {
-        this.setState({
-            step: e.target.value
-        })
-    }
-
-    handleIntervalChange = e => {
-        this.setState({
-            loopInterVal: e.target.value * 1000
-        })
+    handleIntervalChange = event => {
+        const { value } = event.target
+        this.setState({ loopInterVal: value * 1000 })
         clearInterval(this.loopInterval)
-        this.loopInterval = setInterval(this.loopCallback, e.target.value * 1000)
+        this.loopInterval = setInterval(this.loopCallback, value * 1000)
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
     }
 
     enableBtn(ref) {
@@ -125,9 +109,6 @@ export default class Carousel extends React.Component {
     componentDidMount() {
         this.carousel.current.style.width = `${Option.itemWidth * this.state.frameSize}px`
         this.itemsContainer.current.style.width = `${Option.itemWidth * Option.itemsCount}px`
-
-        this.size.current.value = this.state.frameSize
-        this.step.current.value = this.state.step
         this.disableBtn(this.prev)
     }
 
@@ -136,6 +117,10 @@ export default class Carousel extends React.Component {
     }
 
     render() {
+        const options = []
+        for (let i = 1; i <= Option.maxSize; i++) {
+            options.push(<option key={i}>{i}</option>)
+        }
         return (
             <>
                 <div ref={this.carousel} className="Carousel">
@@ -147,62 +132,44 @@ export default class Carousel extends React.Component {
                         ))}
                     </ul>
                 </div>
-                <button
-                    ref={this.prev}
-                    className="btn btn-scroll btn-prev"
-                    onPointerDown={this.scrollBackwards}
-                    type="button"
-                >
-                    &lt;
-                </button>
-                <button
-                    ref={this.next}
-                    className="btn btn-scroll btn-next"
-                    onPointerDown={this.scrollForwards}
-                    type="button"
-                >
-                    &gt;
-                </button>
+                <button ref={this.prev} onPointerDown={this.scrollBackwards} className="btn btn-scroll btn-prev">&lt;</button>
+                <button ref={this.next} onPointerDown={this.scrollForwards} className="btn btn-scroll btn-next">&gt;</button>
                 <br />
-                <button ref={this.loop} className="btn btn-loop" onPointerDown={this.toggleLoop}>
+                <button
+                    ref={this.loop} onPointerDown={this.toggleLoop} className="btn btn-loop"
+                    style={!this.state.infinite ? { boxShadow: 'inset 0 0 5px 2px rgb(134, 134, 11)' } : { boxShadow: '' }}>
                     Loop
                 </button>
                 <br />
                 <br />
-                <label htmlFor="options__frames">Size</label>
-                <select ref={this.size} id="options__frames" onChange={this.handleFramesChange}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-                <label htmlFor="options__step">Step</label>
-                <select ref={this.step} id="options__step" onChange={this.handleStepChange}>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-                {this.state.options && (
+                <label>
+                    Size <select ref={this.size} onChange={this.handleChange} value={this.state.frameSize} name="frameSize">
+                        {options}
+                    </select>
+                </label>
+                
+                <label>
+                    Step <select ref={this.step} onChange={this.handleChange} value={this.state.step} name="step">
+                        {options}
+                    </select>
+                </label>
+                
+                {!this.state.infinite && (
                     <>
-                        <label htmlFor="options__interval">Interval</label>
-                        <select
-                            ref={this.interval}
-                            id="options__interval"
-                            onChange={this.handleIntervalChange}
-                            value={this.state.loopInterVal / 1000}
-                        >
-                            <option>1</option>
-                            <option>2</option>
-                            <option>2.5</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>0.5</option>
-                            <option>0.35</option>
-                        </select>
+                        <label>
+                            Interval <select
+                                ref={this.interval}
+                                onChange={this.handleIntervalChange}
+                                value={this.state.loopInterVal / 1000}
+                                style={{ width: '50px' }}
+                                name="interval"
+                            >
+                                {options}
+                                <option>2.5</option>
+                                <option>0.5</option>
+                                <option>0.35</option>
+                            </select>
+                        </label>
                     </>
                 )}
             </>
