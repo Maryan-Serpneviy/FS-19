30b0fc72756ede7d5b1cae15953e4589623b874e
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-12-12 21:00:10 +0200

    Hooks

--- react_counter/src/Counter.js
+++ react_counter/src/Counter.js
@@ -1,72 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Counter.css';
 
-export default class Counter extends React.Component {
-  constructor()  {
-    super()
+export default function Counter() {
+  const [count, setCount] = useState(0)
 
-    this.state = {
-      count: 0
-    }
+  const increment = () => {
+    setCount(prevCount => prevCount + 1)
   }
 
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
+  const decremenet = () => {
+    setCount(prevCount => prevCount - 1)
   }
 
-  decremenet = () => {
-    this.setState({
-      count: this.state.count - 1
-    })
+  const double = () => {
+    setCount(prevCount => prevCount * 2)
   }
 
-  double = () => {
-    this.setState({
-      count: this.state.count * 2
-    })
+  const halve = () => {
+    setCount(prevCount => prevCount / 2)
   }
 
-  halve = () => {
-    this.setState({
-      count: this.state.count / 2
-    })
+  const pow = () => {
+    setCount(prevCount => prevCount ** 2)
   }
 
-  pow = () => {
-    this.setState({
-      count: this.state.count ** 2
-    })
+  const sqrt = () => {
+    setCount(prevCount => Math.sqrt(prevCount))
   }
 
-  sqrt = () => {
-    this.setState({
-      count: Math.sqrt(this.state.count)
-    })
+  const reset = () => {
+    setCount(prevCount => prevCount = 0)
   }
 
-  reset = () => {
-    this.setState({
-      count: 0
-    })
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <div className="counter__times">{this.state.count}</div>        
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decremenet}>Decrement</button>
-        <br />
-        <button onClick={this.double}>Double</button>
-        <button onClick={this.halve}>Halve</button>
-        <br />
-        <button onClick={this.pow}>Pow</button>
-        <button onClick={this.sqrt}>Sqrt</button>
-        <br />
-        <button onClick={this.reset}>Reset</button>
-      </div>
-    )
-  }
+  return (
+    <div className="counter">
+      <div className="counter__times">{count}</div>        
+      <button onClick={increment}>Increment</button>
+      <button onClick={decremenet}>Decrement</button>
+      <br />
+      <button onClick={double}>Double</button>
+      <button onClick={halve}>Halve</button>
+      <br />
+      <button onClick={pow}>Pow</button>
+      <button onClick={sqrt}>Sqrt</button>
+      <br />
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
 }
