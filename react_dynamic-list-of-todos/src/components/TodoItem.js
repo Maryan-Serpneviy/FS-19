02a2c538f294db42afa9125b11253218c6f7d3d0
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-12-13 20:55:29 +0200

    Hooks

--- react_dynamic-list-of-todos/src/components/TodoItem.js
+++ react_dynamic-list-of-todos/src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import User from './User'
 import getUsers from '../api/users'
 
@@ -42,3 +42,38 @@ export default class TodoItem extends React.Component {
         )
     }
 }
+
+// export default function TodoItem(props) {
+//     const [isLoaded, setLoaded] = useState(false)
+//     const [users, setUsers] = useState([])
+
+//     useEffect(() => {
+//         getUsers.then(data => {
+//             setUsers(data)
+//             setLoaded(true)
+//         })
+//     }, [])
+
+//     if (!isLoaded) {
+//         return <div>Loading...</div>
+//     } else {
+//         return (
+//             <div className="todo">
+//                 {/* task */}
+//                 <h3>{props.task}</h3>
+//                 {/* user */}
+//                 {users.filter(user => props.userId === user.id)
+//                     .map(user => (
+//                         <User
+//                             key={user.id}
+//                             name={user.name}
+//                             username={user.username}
+//                             email={user.email}
+//                             completed={props.completed}
+//                         />
+//                     ))
+//                 }
+//             </div>
+//         )
+//     }
+// }
--- react_dynamic-list-of-todos/src/components/TodoList.js
+++ react_dynamic-list-of-todos/src/components/TodoList.js
@@ -1,60 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoItem from './TodoItem'
 import getTodos from '../api/todos'
 
-export default class TodoList extends React.Component {
-    state = {
-        isRequested: false,
-        isLoaded: false,
-        todos: []
-    }
+export default function TodoList() {
+    const [isRequested, setRequested] = useState(false)
+    const [isLoaded, setLoaded] = useState(false)
+    const [todos, setTodos] = useState([])
 
-    download = () => {
-        this.setState({
-            isRequested: !this.state.isRequested,
-        })
+    function download() {
+        setRequested(true)
 
-        getTodos().then(todos => {
-            this.setState({ isLoaded: true, todos })
+        getTodos().then(data => {
+            setTodos(data)
+            setLoaded(true)
         })
     }
 
-    handleSort = e => {
+    function handleSort(e) {
         const prop = e.target.id
-        this.setState(prev => {
-            return prev.todos.sort((a, b) => {
-                if (a[prop] > b[prop]) return 1
-                else if (a[prop] < b[prop]) return -1
-                return 0
-            })
-        })
+        setTodos([...todos].sort((a, b) => {
+            if (a[prop] > b[prop]) return 1
+            else if (a[prop] < b[prop]) return -1
+            return 0
+        }))
     }
 
-    render() {
-        const { isRequested, isLoaded, todos } = this.state
-
-        if (!isRequested) {
-            return <button onClick={this.download}>Fetch</button>
-        }
-        if (!isLoaded) {
-            return <div>Loading...</div>
-        }
-        return (
-            <div>
-                <button onClick={() => {window.location.reload()}}>Reload</button>
-                <h2>Sort by:</h2>
-                <button id="title" onClick={this.handleSort}>Title</button>
-                <button id="userId" onClick={this.handleSort}>User</button>
-                <button id="completed" onClick={this.handleSort}>Status</button>
-                {todos.map(todo => (
-                    <TodoItem
-                        key={todo.id}
-                        userId={todo.userId}
-                        task={todo.title}
-                        completed={todo.completed}
-                    />
-                ))}
-            </div>
-        )
+    if (!isRequested) {
+        return <button onClick={download}>Fetch</button>
     }
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    }
+    return (
+        <>
+            <button onClick={() => {window.location.reload()}}>Reload</button>
+            <h2>Sort by:</h2>
+            <button id="title" onClick={handleSort}>Title</button>
+            <button id="userId" onClick={handleSort}>User</button>
+            <button id="completed" onClick={handleSort}>Status</button>
+            {todos.map(todo => (
+                <TodoItem
+                    key={todo.id}
+                    userId={todo.userId}
+                    task={todo.title}
+                    completed={todo.completed}
+                />
+            ))}
+        </>
+    )
 }
