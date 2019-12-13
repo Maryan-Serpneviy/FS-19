e5fa355d405881f8e5ca8208520b84893f042b16
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-12-13 21:48:04 +0200

    Hooks

--- react_dynamic-list-of-todos/src/components/TodoItem.js
+++ react_dynamic-list-of-todos/src/components/TodoItem.js
@@ -2,39 +2,33 @@ import React, {useState, useEffect} from 'react'
 import User from './User'
 import getUsers from '../api/users'
 
-export default class TodoItem extends React.Component {
-    state = {
-        isLoaded: false,
-        users: []
-    }
+export default function TodoItem(props) {
+    const [isLoaded, setLoaded] = useState(false)
+    const [users, setUsers] = useState([])
 
-    componentDidMount() {
-        getUsers().then(users => {
-            this.setState({
-                isLoaded: !this.state.isLoaded,
-                users
-            })
+    useEffect(() => {
+        getUsers().then(data => {
+            setUsers(data)
+            setLoaded(true)
         })
-    }
+    }, [])
 
-    render() {
-        if (!this.state.isLoaded) {
-            return <div>Loading...</div>
-        }
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    } else {
         return (
             <div className="todo">
                 {/* task */}
-                <h3>{this.props.task}</h3>
+                <h3>{props.task}</h3>
                 {/* user */}
-                {this.state.users
-                    .filter(user => this.props.userId === user.id)
+                {users.filter(user => props.userId === user.id)
                     .map(user => (
                         <User
                             key={user.id}
                             name={user.name}
                             username={user.username}
                             email={user.email}
-                            completed={this.props.completed}
+                            completed={props.completed}
                         />
                     ))
                 }
@@ -42,38 +36,3 @@ export default class TodoItem extends React.Component {
         )
     }
 }
-
-// export default function TodoItem(props) {
-//     const [isLoaded, setLoaded] = useState(false)
-//     const [users, setUsers] = useState([])
-
-//     useEffect(() => {
-//         getUsers.then(data => {
-//             setUsers(data)
-//             setLoaded(true)
-//         })
-//     }, [])
-
-//     if (!isLoaded) {
-//         return <div>Loading...</div>
-//     } else {
-//         return (
-//             <div className="todo">
-//                 {/* task */}
-//                 <h3>{props.task}</h3>
-//                 {/* user */}
-//                 {users.filter(user => props.userId === user.id)
-//                     .map(user => (
-//                         <User
-//                             key={user.id}
-//                             name={user.name}
-//                             username={user.username}
-//                             email={user.email}
-//                             completed={props.completed}
-//                         />
-//                     ))
-//                 }
-//             </div>
-//         )
-//     }
-// }
